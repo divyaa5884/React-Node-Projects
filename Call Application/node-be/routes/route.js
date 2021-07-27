9bd6447d6b48dbb2647bb6741d401a08e59b3d2a
Divya <divyaa5884@gmail.com>
2021-07-28 03:23:30 +0530

    pushed deleted folders

--- Call Application/node-be/routes/route.js
+++
@@ -1,6 +0,0 @@
-const Router = require('express').Router();
-const Controllers = require('./../controllers/controller')
-
-Router.post('/connectCall', Controllers.userCreate, Controllers.logCreate);
-
-module.exports = Router;
